7dfec0484b452de5f6106a3b74b8c6291f142cf7
4868050801 <alex.quantschnig@htl-klu.at>
2022-01-06 11:59:27 +0100

    Clean Badge

---
+++ lib/datadisplay/badge/badgeContainer.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:legend_design_core/typography/typography.dart';
+import 'package:legend_design_core/typography/legend_text.dart';
+
+class BadgeContainer extends StatelessWidget {
+  final int? count;
+  final String? text;
+  final double? height;
+  final Color badgeColor;
+  const BadgeContainer({
+    this.count,
+    this.text,
+    this.height,
+    required this.badgeColor,
+  });
+
+  String getData() {
+    if (count != null) {
+      return "$count";
+    } else if (text != null) {
+      return text!;
+    } else {
+      return "";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(horizontal: 4),
+      height: height,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+        color: badgeColor,
+      ),
+      child: LegendText(
+        textStyle: LegendTextStyle(fontSize: 8),
+        text: getData(),
+      ),
+    );
+  }
+}
---
+++ lib/datadisplay/badge/legendBadge.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:legend_design_core/typography/typography.dart';
+import 'package:legend_design_widgets/datadisplay/badge/badgeContainer.dart';
+
+enum LegendBadgeValues { Text, Count, Dot }
+
+class LegendBadge extends StatelessWidget {
+  late final LegendBadgeValues value;
+  LegendTextStyle? typography;
+  String? text;
+  int? count;
+  final Widget badgeWidget;
+  double? height;
+  Color badgeColor;
+
+  LegendBadge.text({
+    required this.text,
+    required this.typography,
+    required this.badgeWidget,
+    this.height,
+    required this.badgeColor,
+  }) {
+    value = LegendBadgeValues.Text;
+  }
+
+  LegendBadge.count({
+    required this.count,
+    required this.badgeWidget,
+    this.height,
+    required this.badgeColor,
+  }) {
+    value = LegendBadgeValues.Count;
+  }
+  LegendBadge.dot({
+    required this.badgeColor,
+    required this.badgeWidget,
+  }) {
+    value = LegendBadgeValues.Dot;
+  }
+
+  Widget getBadge() {
+    switch (value) {
+      case LegendBadgeValues.Text:
+        return BadgeContainer(
+          text: text,
+          height: height,
+          badgeColor: badgeColor,
+        );
+      case LegendBadgeValues.Count:
+        return BadgeContainer(
+          height: height,
+          count: count,
+          badgeColor: badgeColor,
+        );
+      case LegendBadgeValues.Dot:
+        return Container(
+          width: 12,
+          height: 12,
+          decoration: BoxDecoration(
+            color: badgeColor,
+            shape: BoxShape.circle,
+          ),
+        );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double inset = 6;
+
+    if (height != null) {
+      inset = height! / 2;
+    }
+    return Container(
+      padding: EdgeInsets.only(top: inset, right: inset),
+      child: Stack(
+        children: [
+          badgeWidget,
+          Positioned(
+            top: 0.0,
+            right: 0.0,
+            child: Container(
+              transform: Matrix4.translationValues(inset, -inset, 0),
+              child: getBadge(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
