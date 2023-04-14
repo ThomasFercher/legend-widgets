269f46a0ce3970bed4da313350718b3d82546fed
Alex-design01 <alex.quantschnig@gmx.at>
2023-04-14 16:04:07 +0200

    optimized

--- lib/datadisplay/badge/legendBadge.dart
+++ lib/datadisplay/badge/legendBadge.dart
@@ -3,81 +3,18 @@ import 'package:legend_design_core/styles/typography/typography.dart';
 
 import 'package:legend_design_widgets/datadisplay/badge/badgeContainer.dart';
 
-enum LegendBadgeValues { Text, Count, Dot, Icon }
-
 class LegendBadge extends StatelessWidget {
-  late final LegendBadgeValues value;
-  TextStyle? typography;
-  String? text;
-  int? count;
-  final Widget badgeWidget;
-  double? height;
-  Color badgeColor;
-  IconButton? icon;
+  final Widget content;
+  final double? height;
+  final Color badgeColor;
+  final Widget badge;
 
-  LegendBadge.text({
-    required this.text,
-    required this.typography,
-    required this.badgeWidget,
+  LegendBadge({
+    required this.content,
     this.height,
-    required this.badgeColor,
-  }) {
-    value = LegendBadgeValues.Text;
-  }
-
-  LegendBadge.count({
-    required this.count,
-    required this.badgeWidget,
-    this.height,
-    required this.badgeColor,
-  }) {
-    value = LegendBadgeValues.Count;
-  }
-  LegendBadge.dot({
-    required this.badgeColor,
-    required this.badgeWidget,
-  }) {
-    value = LegendBadgeValues.Dot;
-  }
-  LegendBadge.icon({
-    required this.icon,
-    required this.badgeColor,
-    required this.badgeWidget,
-  }) {
-    value = LegendBadgeValues.Icon;
-  }
-
-  Widget getBadge() {
-    switch (value) {
-      case LegendBadgeValues.Text:
-        return BadgeContainer(
-          text: text,
-          height: height,
-          badgeColor: badgeColor,
-        );
-      case LegendBadgeValues.Count:
-        return BadgeContainer(
-          height: height,
-          count: count,
-          badgeColor: badgeColor,
-        );
-      case LegendBadgeValues.Dot:
-        return Container(
-          width: 12,
-          height: 12,
-          decoration: BoxDecoration(
-            color: badgeColor,
-            shape: BoxShape.circle,
-          ),
-        );
-      case LegendBadgeValues.Icon:
-        return Container(
-          width: 15,
-          height: 15,
-          child: icon,
-        );
-    }
-  }
+    this.badgeColor = Colors.red,
+    this.badge = const _DotBadge(),
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -90,16 +27,34 @@ class LegendBadge extends StatelessWidget {
       padding: EdgeInsets.only(top: inset, right: inset),
       child: Stack(
         children: [
-          badgeWidget,
+          content,
           Positioned(
             top: 0.0,
             right: 0.0,
             child: Container(
               transform: Matrix4.translationValues(inset, -inset, 0),
-              child: getBadge(),
+              child: badge,
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _DotBadge extends StatelessWidget {
+  const _DotBadge({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 12,
+      width: 12,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        shape: BoxShape.circle,
       ),
     );
   }
